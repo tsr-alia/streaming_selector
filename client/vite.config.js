8d612ca25f72235262542e5710a8c3b725cc40cb
tsr-alia <kontakt@annakliemann.net>
2024-10-09 19:43:50 +0200

    added Atlas database and set up concurrently in root directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for the build
+    sourcemap: false, // Disable source maps in production
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  }
 })
